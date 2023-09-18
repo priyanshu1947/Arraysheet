9b3b4264e2fc2277312c08a0f4c2ef5485a2fc7b
priyanshu1947 <70458921+priyanshu1947@users.noreply.github.com>
2023-09-18 12:10:25 +0530

    Time: 0 ms (100%), Space: 44.3 MB (34.03%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,43 +1,15 @@
 class Solution {
-    public int missingNumber(int[] arr) 
-    {
-        // no from 0 to n = so there will be total n+1 nos.
-        // ex N=4, arr [4,0,2,1]
-        // [0,1,2,3,4] so missing no is 3;
-        // here in this question is given from 0 to n. so the index value and the elemnet at index will also be same like at index 1 the element will also be 1.
-
-        int i = 0;
-        while(i<arr.length)
-        {
-            int check = arr[i];
-            if(arr[i]<arr.length && arr[i] !=arr[check])
-            {
-            swap(arr,i,check);
-            }
-            else
-            {
-                i++;
-            }
-        }
-        // search for first missing number
-        for (int index = 0; index < arr.length;index++)
-        {
-            if(arr[index]!= index)
-            {
-                return index;
-            }
-        }
-
-        //case 2
-        return arr.length;
-
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
         
+        // Initialize the result with n, as it's the maximum possible missing number.
+        int result = n;
+        
+        // XOR all elements in arr and XOR with numbers from 0 to n.
+        for (int i = 0; i < n; i++) {
+            result =result^ i ^ arr[i];
+        }
+        
+        return result;
     }
-    public void swap(int [] arr, int first, int second)
-    {
-        int temp = arr[first];
-        arr[first]= arr[second];
-        arr[second]= temp;
-    }
-
 }
